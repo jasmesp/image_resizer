f90248aee249a940dccd6d181689a8e32af8929c
Jas Palermo <jasmes@gmail.com>
2023-12-03 10:51:54 -0500

    Add .gitignore file and improve image resizing script
    .gitignore file is added to ignore non-essential generated files which should not be tracked by GIT.
    
    In 'main.py', made amendments to homogenize filenames and handle file existence errors for thumbnail directory function. Also improved readability of the code and reduced the redundancy in the code.
    
    In '.idea/misc.xml', updated the 'component' parameters to better suit the project requirements.
    
    In '.idea/vcs.xml', added a mapping directory which now uses GIT as the version control system.
    
    These changes are made to streamline the code and versioning process.

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 import os
-import time
+
 from PIL import Image
 
-#get out gooey package
+
 def make_thumbnail_dir():
-    root_output_dir = './imgs_out'
-    make_thumbnail_dir = '/thumbnail'
-    os.mkdir(root_output_dir + make_thumbnail_dir + '/')
-    return root_output_dir + make_thumbnail_dir + '/'
+    root_output_dir = "./imgs_out/"
+    thumbnail_dir = "/thumbnail"
+    thumbnail_path = root_output_dir + thumbnail_dir + "/"
+
+    try:
+        if not os.path.exists(root_output_dir):
+            os.mkdir(root_output_dir)
+
+        os.mkdir(thumbnail_path)
+    except FileExistsError:
+        pass
+
+    return thumbnail_path
 
 
-imgs_dir = './imgs'
+def homogenize_filenames():
+    import os
+
+    imgs_dir = "./imgs"
+
+    for file in os.listdir(imgs_dir):
+        if file.upper().endswith((".JPG", ".JPEG", ".PNG")):
+            os.rename(
+                os.path.join(imgs_dir, file), os.path.join(imgs_dir, file.lower())
+            )
+
+
+imgs_dir = "./imgs"
 imgs_out_dir = make_thumbnail_dir()
 
-
 # os.listdir(imgs_dir)
-
+homogenize_filenames()
 for file in os.listdir(imgs_dir):
-    if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".JPG"):
-        img = Image.open(imgs_dir + '/' + file)
+    if file.lower().endswith((".jpg", ".jpeg", ".png")):
+        img = Image.open(imgs_dir + "/" + file)
         output_width = 300
-        width_percentage = (output_width / float(img.size[0]))
+        width_percentage = output_width / float(img.size[0])
         output_height = int((float(img.size[1]) * float(width_percentage)))
         img = img.resize((output_width, output_height))
-        img.save(imgs_out_dir + 'thumb_' + file)
+        img.save(imgs_out_dir + "thumb_" + file.lower())
         print(file)
     else:
         print("Not a jpg")
